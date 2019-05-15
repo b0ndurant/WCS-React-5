bc94d7bfcd6a2891157c1efb620094f3d6433cc0
b0ndurant <martin.aurelien01@gmail.com>
2019-05-15 18:20:58 +0200

    Updates

---
+++ precache-manifest.8363873cb1b84f56c5e50a7791251caf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3ee5355b04a8f8a6a7f000f31bad1c",
+    "url": "/WCS-React-5/index.html"
+  },
+  {
+    "revision": "f5395efde46a05e27189",
+    "url": "/WCS-React-5/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/WCS-React-5/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f5395efde46a05e27189",
+    "url": "/WCS-React-5/static/js/main.0dffa9ba.chunk.js"
+  },
+  {
+    "revision": "3ef7f78284dd0315fa63",
+    "url": "/WCS-React-5/static/js/runtime~main.3de995c6.js"
+  }
+]);
